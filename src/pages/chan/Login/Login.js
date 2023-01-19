55409c9a1c5fb3e1ceb99445289da793c654bd9e
seungchannn <121918663+seungchannn@users.noreply.github.com>
2023-01-19 20:39:38 +0900

    creat project

--- src/pages/chan/Login/Login.js
+++ src/pages/chan/Login/Login.js
@@ -1,6 +1,46 @@
-import React from 'react';
-const Login = () => {
-  return <h1>Login Page</h1>;
-};
+// import React from "react";
+import { useNavigate } from 'react-router-dom';
+import './Login.scss';
+
+function Login() {
+  const navigate = useNavigate();
+  return (
+    <div className="Login">
+      <main>
+        <form className="login">
+          <h1 className="login-title">Westagram</h1>
+
+          <input
+            id="id"
+            name="id"
+            type="text"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="비밀번호"
+          />
+
+          <button
+            onClick={() => {
+              navigate('/main');
+            }}
+            className="btn"
+          >
+            로그인
+          </button>
+          <div className="login-signup">
+            <a className="login-signup-link" href="#">
+              비밀번호를 잊으셨나요 ?
+            </a>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
+}
 
 export default Login;
