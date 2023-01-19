9d6a438dd76ea2e63d86a3214bb49f311d8e9e04
gmgmgun <gmgmgun@gmail.com>
2023-01-19 14:27:33 +0900

    add: Login 및 Main 페이지 이전 작업 내용 추가
    public 디렉토리 내 필요한 바이너리 파일 추가
    Login.js -> 로그인 버튼에 Nav hook 기능 추가
    Login.scss -> 네스팅 작업 완료
    Main.scss -> 네스팅 작업 완료(컴포넌트 추가 시 수정 필요)
    
    보완해야하는 사항 -> Login 및 Main 페이지 엘리먼트 사이즈 및 간격 조절

--- src/pages/dongmin/Login/Login.js
+++ src/pages/dongmin/Login/Login.js
@@ -1,6 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Login.scss';
+
 const Login = () => {
-  return <h1>Login Page</h1>;
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  const changeId = e => {
+    setId(e.target.value);
+  };
+  const changePw = e => {
+    setPw(e.target.value);
+  };
+  const isValid = id.length === 0 || pw.length === 0 ? false : true;
+  const isBtnActive = isValid ? 'button-abled' : 'button-disabled';
+
+  const navigate = useNavigate();
+  const onClickButton = () => {
+    return navigate('/main-dongmin');
+  };
+
+  return (
+    <div className="main-cnt">
+      <h1>westagram</h1>
+      <div className="input-cnt">
+        <input
+          type="text"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+          className="input-id"
+          onChange={changeId}
+        />
+        <input
+          type="password"
+          placeholder="비밀번호"
+          className="input-pw"
+          onChange={changePw}
+        />
+      </div>
+      <button className={isBtnActive} onClick={onClickButton}>
+        로그인
+      </button>
+      <a href="https://www.instagram.com/accounts/password/reset/">
+        비밀번호를 잊으셨나요?
+      </a>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/dongmin/Main/Main.js
+++ src/pages/dongmin/Main/Main.js
@@ -1,6 +1,329 @@
 import React from 'react';
+import './Main.scss';
+
 const Main = () => {
-  return <h1>Main Page</h1>;
+  return (
+    <>
+      <Nav />
+      <main>
+        <Feeds />
+        <MainRight />
+      </main>
+    </>
+  );
+};
+
+const Nav = () => {
+  return (
+    <nav>
+      <div className="nav-cnt">
+        <ul className="nav-left">
+          <li>
+            <img
+              className="nav-item img-logo"
+              alt="logo"
+              src={process.env.PUBLIC_URL + '/images/dongmin/logo.png'}
+            />
+          </li>
+          <li className="nav-item line-vertical">|</li>
+          <li>
+            <h1 className="nav-item">Westagram</h1>
+          </li>
+        </ul>
+        <input className="nav-center" type="text" placeholder="검색" />
+        <ul className="nav-right">
+          <li>
+            <img
+              className="nav-item"
+              alt="logo"
+              src={process.env.PUBLIC_URL + '/images/dongmin/compass.png'}
+            />
+          </li>
+          <li>
+            <img
+              className="nav-item"
+              alt="logo"
+              src={process.env.PUBLIC_URL + '/images/dongmin/empty-heart.png'}
+            />
+          </li>
+          <li>
+            <img
+              className="nav-item"
+              alt="logo"
+              src={process.env.PUBLIC_URL + '/images/dongmin/user.png'}
+            />
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+const Feeds = () => {
+  return (
+    <div className="feeds">
+      <article>
+        <div className="profile-cnt">
+          <img
+            className="profile-img"
+            alt="logo"
+            src={process.env.PUBLIC_URL + '/images/dongmin/wecode.png'}
+          />
+          <ul className="profile-text">
+            <li>wecode_bootcamp</li>
+            <li>WeCode - 위코드</li>
+          </ul>
+        </div>
+        <div className="img-focus-cnt">
+          <img
+            className="img-focus"
+            alt="focus"
+            src={process.env.PUBLIC_URL + '/images/dongmin/feed.jpg'}
+          />
+        </div>
+        <div className="button-cnt">
+          <ul className="icon-cnt">
+            <li>
+              <button className="heart-button">
+                <img
+                  className="article-icon"
+                  alt="logo"
+                  src={
+                    process.env.PUBLIC_URL + '/images/dongmin/empty-heart.png'
+                  }
+                />
+              </button>
+            </li>
+            <li>
+              <button>
+                <img
+                  className="article-icon"
+                  alt="logo"
+                  src={process.env.PUBLIC_URL + '/images/dongmin/chat.png'}
+                />
+              </button>
+            </li>
+            <li>
+              <button>
+                <img
+                  className="article-icon"
+                  alt="logo"
+                  src={process.env.PUBLIC_URL + '/images/dongmin/upload.png'}
+                />
+              </button>
+            </li>
+          </ul>
+          <div className="icon-cnt">
+            <button>
+              <img
+                className="article-icon"
+                alt="logo"
+                src={process.env.PUBLIC_URL + '/images/dongmin/bookmark.png'}
+              />
+            </button>
+          </div>
+        </div>
+
+        <div id="like-amount" className="like-amount-not-zero">
+          <div className="like-cnt">
+            <div className="like-profile-cnt">
+              <img
+                className="like-profile-img"
+                alt="logo"
+                src={process.env.PUBLIC_URL + '/images/dongmin/wecode.png'}
+              />
+              <div className="like-profile-text">wecode_bootcamp</div>
+            </div>
+            <div>님이 좋아합니다.</div>
+          </div>
+        </div>
+        <div className="name-content-cnt size-for-overflow-hidden">
+          <span className="name">chanho_park</span>
+          <span className="content">
+            제가 LA에 있을때는 말이죠 정말
+            제가나다라마바사아자카타파하아자차카타파하
+          </span>
+        </div>
+        <div>
+          <ul className="comment-li"></ul>
+        </div>
+        <hr />
+        <div className="input-cnt">
+          <input
+            className="input-comment"
+            type="text"
+            placeholder="댓글 달기.."
+          />
+          <button className="submit">게시</button>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+const MainRight = () => {
+  return (
+    <div className="main-right">
+      <aside>
+        <div className="profile-cnt">
+          <img
+            className="profile-img"
+            alt="logo"
+            src={process.env.PUBLIC_URL + '/images/dongmin/wecode.png'}
+          />
+          <ul className="profile-text">
+            <li>wecode_bootcamp</li>
+            <li>WeCode - 위코드</li>
+          </ul>
+        </div>
+        <div className="story-wrap">
+          스토리
+          <span className="show-all">모두 보기</span>
+          <ul className="story-li">
+            <li>
+              <div className="side-profile-cnt">
+                <img
+                  className="side-profile-img"
+                  alt="logo"
+                  src={process.env.PUBLIC_URL + '/images/dongmin/junhyun.jpg'}
+                />
+                <ul className="side-profile-text">
+                  <li>junwise.park</li>
+                  <li>16분 전</li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <div className="side-profile-cnt">
+                <img
+                  className="side-profile-img"
+                  alt="logo"
+                  src={process.env.PUBLIC_URL + '/images/dongmin/junhyun.jpg'}
+                />
+                <ul className="side-profile-text">
+                  <li>junwise.park</li>
+                  <li>3시간 전</li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <div className="side-profile-cnt">
+                <img
+                  className="side-profile-img"
+                  alt="logo"
+                  src={process.env.PUBLIC_URL + '/images/dongmin/junhyun.jpg'}
+                />
+                <ul className="side-profile-text">
+                  <li>junwise.park</li>
+                  <li>6시간 전</li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <div className="side-profile-cnt">
+                <img
+                  className="side-profile-img"
+                  alt="logo"
+                  src={process.env.PUBLIC_URL + '/images/dongmin/junhyun.jpg'}
+                />
+                <ul className="side-profile-text">
+                  <li>junwise.park</li>
+                  <li>12시간 전</li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="recommend-wrap">
+          회원님을 위한 추천
+          <span className="show-all">모두 보기</span>
+          <ul className="story-li">
+            <li>
+              <div className="side-profile-cnt">
+                <img
+                  className="side-profile-img"
+                  alt="logo"
+                  src={process.env.PUBLIC_URL + '/images/dongmin/soohyun.png'}
+                />
+                <ul className="side-profile-text">
+                  <li>watervillage.kim</li>
+                  <li>dongmin.lee 외 2명이 알고 있을지도 모르고 있을지도</li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <div className="side-profile-cnt">
+                <img
+                  className="side-profile-img"
+                  alt="logo"
+                  src={process.env.PUBLIC_URL + '/images/dongmin/soohyun.png'}
+                />
+                <ul className="side-profile-text">
+                  <li>watervillage.kim</li>
+                  <li>dongmin.lee 외 2명이 알고 있을지도 모르고 있을지도</li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <div className="side-profile-cnt">
+                <img
+                  className="side-profile-img"
+                  alt="logo"
+                  src={process.env.PUBLIC_URL + '/images/dongmin/soohyun.png'}
+                />
+                <ul className="side-profile-text">
+                  <li>watervillage.kim</li>
+                  <li>dongmin.lee 외 2명이 알고 있을지도 모르고 있을지도</li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <div className="side-profile-cnt">
+                <img
+                  className="side-profile-img"
+                  alt="logo"
+                  src={process.env.PUBLIC_URL + '/images/dongmin/soohyun.png'}
+                />
+                <ul className="side-profile-text">
+                  <li>watervillage.kim</li>
+                  <li>dongmin.lee 외 2명이 알고 있을지도 모르고 있을지도</li>
+                </ul>
+              </div>
+            </li>
+            <li>
+              <div className="side-profile-cnt">
+                <img
+                  className="side-profile-img"
+                  alt="logo"
+                  src={process.env.PUBLIC_URL + '/images/dongmin/soohyun.png'}
+                />
+                <ul className="side-profile-text">
+                  <li>watervillage.kim</li>
+                  <li>dongmin.lee 외 2명이 알고 있을지도 모르고 있을지도</li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </aside>
+      <footer>
+        <ul>
+          <li>Instagram 정보</li>
+          <li>지원</li>
+          <li>홍보 센터</li>
+          <li>API</li>
+          <li>채용 정보</li>
+          <li>개인정보처리방침</li>
+          <li>약관</li>
+          <li>디렉터리</li>
+          <li>프로필</li>
+          <li>해시태그</li>
+          <li>언어</li>
+        </ul>
+        <span>&copy; 2023 INSTAGRAM</span>
+      </footer>
+    </div>
+  );
 };
 
 export default Main;
