2d279b84f0eb973372c9022ab14c7bd17d0bf77c
gmgmgun <gmgmgun@gmail.com>
2023-01-30 23:45:58 +0900

    상수 데이터 사용해 map 구현. MainRight 컴포넌트->ProfileCard 컴포넌트화

--- src/pages/dongmin/Main/MainRight/MainRight.js
+++ src/pages/dongmin/Main/MainRight/MainRight.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import ProfileCard from './Components/ProfileCard';
 import './MainRight.scss';
-
 const MainRight = () => {
+  const setProfileCard = profile => {
+    const profileArr = [];
+    for (let i = 0; i <= 5; i++) {
+      profileArr.push(profile);
+    }
+    return profileArr;
+  };
   return (
     <div className="mainRight">
       <aside>
@@ -9,7 +16,7 @@ const MainRight = () => {
           <img
             className="profileImg"
             alt="logo"
-            src={process.env.PUBLIC_URL + '/images/dongmin/wecode.png'}
+            src="/images/dongmin/wecode.png"
           />
           <ul className="profileText">
             <li>wecode_bootcamp</li>
@@ -20,196 +27,28 @@ const MainRight = () => {
           스토리
           <span className="showAll">모두 보기</span>
           <ul className="storyLi">
-            <li>
-              <div className="sideProfileCnt">
-                <img
-                  className="sideProfileImg"
-                  alt="logo"
-                  src={process.env.PUBLIC_URL + '/images/dongmin/junhyun.jpg'}
-                />
-                <ul className="sideProfileText">
-                  <li>junwise.park</li>
-                  <li>16분 전</li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <div className="sideProfileCnt">
-                <img
-                  className="sideProfileImg"
-                  alt="logo"
-                  src={process.env.PUBLIC_URL + '/images/dongmin/junhyun.jpg'}
-                />
-                <ul className="sideProfileText">
-                  <li>junwise.park</li>
-                  <li>3시간 전</li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <div className="sideProfileCnt">
-                <img
-                  className="sideProfileImg"
-                  alt="logo"
-                  src={process.env.PUBLIC_URL + '/images/dongmin/junhyun.jpg'}
-                />
-                <ul className="sideProfileText">
-                  <li>junwise.park</li>
-                  <li>3시간 전</li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <div className="sideProfileCnt">
-                <img
-                  className="sideProfileImg"
-                  alt="logo"
-                  src={process.env.PUBLIC_URL + '/images/dongmin/junhyun.jpg'}
-                />
-                <ul className="sideProfileText">
-                  <li>junwise.park</li>
-                  <li>3시간 전</li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <div className="sideProfileCnt">
-                <img
-                  className="sideProfileImg"
-                  alt="logo"
-                  src={process.env.PUBLIC_URL + '/images/dongmin/junhyun.jpg'}
-                />
-                <ul className="sideProfileText">
-                  <li>junwise.park</li>
-                  <li>6시간 전</li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <div className="sideProfileCnt">
-                <img
-                  className="sideProfileImg"
-                  alt="logo"
-                  src={process.env.PUBLIC_URL + '/images/dongmin/junhyun.jpg'}
-                />
-                <ul className="sideProfileText">
-                  <li>junwise.park</li>
-                  <li>12시간 전</li>
-                </ul>
-              </div>
-            </li>
+            {PROFILE_INFO_LIST[0].map(info => (
+              <ProfileCard
+                key={info.id}
+                name={info.name}
+                nickname={info.nickname}
+                time={info.time}
+              />
+            ))}
           </ul>
         </div>
         <div className="recommendWrap">
           회원님을 위한 추천
           <span className="showAll">모두 보기</span>
           <ul className="storyLi">
-            <li>
-              <div className="sideProfileCnt">
-                <img
-                  className="sideProfileImg"
-                  alt="logo"
-                  src={process.env.PUBLIC_URL + '/images/dongmin/soohyun.png'}
-                />
-                <ul className="sideProfileText">
-                  <li>watervillage.kim</li>
-                  <li>dongmin.lee 외 2명이 알고 있을지도 모르고 있을지도</li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <div className="sideProfileCnt">
-                <img
-                  className="sideProfileImg"
-                  alt="logo"
-                  src={process.env.PUBLIC_URL + '/images/dongmin/soohyun.png'}
-                />
-                <ul className="sideProfileText">
-                  <li>watervillage.kim</li>
-                  <li>dongmin.lee 외 2명이 알고 있을지도 모르고 있을지도</li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <div className="sideProfileCnt">
-                <img
-                  className="sideProfileImg"
-                  alt="logo"
-                  src={process.env.PUBLIC_URL + '/images/dongmin/soohyun.png'}
-                />
-                <ul className="sideProfileText">
-                  <li>watervillage.kim</li>
-                  <li>dongmin.lee 외 2명이 알고 있을지도 모르고 있을지도</li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <div className="sideProfileCnt">
-                <div className="box">
-                  <img
-                    className="sideProfileImg"
-                    alt="logo"
-                    src={process.env.PUBLIC_URL + '/images/dongmin/soohyun.png'}
-                  />
-                </div>
-                <ul className="sideProfileText">
-                  <li>watervillage.kim</li>
-                  <li>dongmin.lee 외 2명이 알고 있을지도 모르고 있을지도</li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <div className="sideProfileCnt">
-                <img
-                  className="sideProfileImg"
-                  alt="logo"
-                  src={process.env.PUBLIC_URL + '/images/dongmin/soohyun.png'}
-                />
-                <ul className="sideProfileText">
-                  <li>watervillage.kim</li>
-                  <li>dongmin.lee 외 2명이 알고 있을지도 모르고 있을지도</li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <div className="sideProfileCnt">
-                <img
-                  className="sideProfileImg"
-                  alt="logo"
-                  src={process.env.PUBLIC_URL + '/images/dongmin/soohyun.png'}
-                />
-                <ul className="sideProfileText">
-                  <li>watervillage.kim</li>
-                  <li>dongmin.lee 외 2명이 알고 있을지도 모르고 있을지도</li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <div className="sideProfileCnt">
-                <img
-                  className="sideProfileImg"
-                  alt="logo"
-                  src={process.env.PUBLIC_URL + '/images/dongmin/soohyun.png'}
-                />
-                <ul className="sideProfileText">
-                  <li>watervillage.kim</li>
-                  <li>dongmin.lee 외 2명이 알고 있을지도 모르고 있을지도</li>
-                </ul>
-              </div>
-            </li>
-            <li>
-              <div className="sideProfileCnt">
-                <img
-                  className="sideProfileImg"
-                  alt="logo"
-                  src={process.env.PUBLIC_URL + '/images/dongmin/soohyun.png'}
-                />
-                <ul className="sideProfileText">
-                  <li>watervillage.kim</li>
-                  <li>dongmin.lee 외 2명이 알고 있을지도 모르고 있을지도</li>
-                </ul>
-              </div>
-            </li>
+            {PROFILE_INFO_LIST[1].map(info => (
+              <ProfileCard
+                key={info.id}
+                name={info.name}
+                nickname={info.nickname}
+                time={info.time}
+              />
+            ))}
           </ul>
         </div>
       </aside>
@@ -234,3 +73,24 @@ const MainRight = () => {
 };
 
 export default MainRight;
+
+const PROFILE_INFO_LIST = [
+  [
+    { id: 1, name: 'junhyun', nickname: 'junwise.park', time: '24초 전' },
+    { id: 2, name: 'junhyun', nickname: 'junwise.park', time: '42초 전' },
+    { id: 3, name: 'junhyun', nickname: 'junwise.park', time: '24분 전' },
+    { id: 4, name: 'junhyun', nickname: 'junwise.park', time: '42분 전' },
+    { id: 5, name: 'junhyun', nickname: 'junwise.park', time: '43분 전' },
+    { id: 6, name: 'junhyun', nickname: 'junwise.park', time: '44분 전' },
+    { id: 7, name: 'junhyun', nickname: 'junwise.park', time: '45분 전' },
+  ],
+  [
+    { id: 1, name: 'soohyun', nickname: 'watervillage.kim', time: '24초 전' },
+    { id: 2, name: 'soohyun', nickname: 'watervillage.kim', time: '42초 전' },
+    { id: 3, name: 'soohyun', nickname: 'watervillage.kim', time: '24분 전' },
+    { id: 4, name: 'soohyun', nickname: 'watervillage.kim', time: '42분 전' },
+    { id: 5, name: 'soohyun', nickname: 'watervillage.kim', time: '43분 전' },
+    { id: 6, name: 'soohyun', nickname: 'watervillage.kim', time: '44분 전' },
+    { id: 7, name: 'soohyun', nickname: 'watervillage.kim', time: '45분 전' },
+  ],
+];
