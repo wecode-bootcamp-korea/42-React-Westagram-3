385fc983d7228ec40934cbb5bc5f39c3a65af00b
gmgmgun <gmgmgun@gmail.com>
2023-01-31 18:59:56 +0900

    Add: Nav 컴포넌트 Merge 후 CSS 수정

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,39 +4,39 @@ import './Nav.scss';
 const Nav = () => {
   return (
     <nav>
-      <div className="nav-cnt">
-        <ul className="nav-left">
+      <div className="navCnt">
+        <ul className="navLeft">
           <li>
             <img
-              className="nav-item img-logo"
+              className="navItem"
               alt="logo"
               src="/images/dongmin/logo.png"
             />
           </li>
-          <li className="nav-item line-vertical">|</li>
+          <li className="navItem lineVertical">|</li>
           <li>
-            <h1 className="nav-item">Westagram</h1>
+            <h1 className="navItem">Westagram</h1>
           </li>
         </ul>
-        <input className="nav-center" type="text" placeholder="검색" />
-        <ul className="nav-right">
+        <input className="navCenter" type="text" placeholder="검색" />
+        <ul className="navRight">
           <li>
             <img
-              className="nav-item"
+              className="navItem"
               alt="logo"
               src="/images/dongmin/compass.png"
             />
           </li>
           <li>
             <img
-              className="nav-item"
+              className="navItem"
               alt="logo"
               src="/images/dongmin/empty-heart.png"
             />
           </li>
           <li>
             <img
-              className="nav-item"
+              className="navItem"
               alt="logo"
               src="/images/dongmin/user.png"
             />
